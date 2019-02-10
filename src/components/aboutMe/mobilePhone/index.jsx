3eb1f6681a89a58ab9b95424ba5426f86f4a8758
Simone <panyuximeng@163.com>
2019-02-10 20:58:16 +0800

    移动端个人信息

--- src/components/aboutMe/mobilePhone/index.jsx
+++ src/components/aboutMe/mobilePhone/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import httpAjax from 'httpAjax';
 import style from './style.scss';
 
 
@@ -6,6 +7,22 @@ class AboutMe extends React.Component{
     constructor(props){
         super(props);
         this.nodeMe = React.createRef();
+        this.state = {
+            content:""
+        }
+    }
+
+    componentDidMount(){
+        httpAjax.ajax('/meContent' + '?contentId=0').then(res=>{
+                this.setState({
+                    content:res.data.data.articleContent
+                },()=>{
+                    this.nodeMe.current.style.marginTop = 0;
+                    this.nodeMe.current.style.opacity = 1;
+                })
+        }).catch(err=>{
+            console.error(err);
+        })
     }
 
     backTo = () => {
@@ -14,13 +31,13 @@ class AboutMe extends React.Component{
 
     render(){
         return (
-            <div ref={this.nodeMe} className={style['about-me-div']}>
+            <div className={style['about-me-div']}>
                 <div className={style['header-box']}>
                     <span onClick={this.backTo} className="iconfont">&#xe606;</span>
                     <h2 className={style['person-resume']}>个人简介</h2>
                 </div>
-                <div>
-                    文字显示文字显示文字显示文字显示文字显示文字显示文字显示文字显示
+                <div ref={this.nodeMe} className={style['about-content']}>
+                    <div className={style['about-content-inner']} dangerouslySetInnerHTML={{__html:this.state.content}}></div>
                 </div>
             </div>
         )
